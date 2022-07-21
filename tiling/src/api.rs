c2ab72fe3d0cb2d08d02b05729c5586931410303
Nicolas Silva <nical@fastmail.com>
2022-07-21 22:25:22 +0200

    A somewhat less hacky api

---
+++ tiling/src/api.rs
@@ -0,0 +1,333 @@
+use std::sync::Arc;
+use lyon::path::Path;
+use lyon::geom::euclid::default::{Transform2D, Box2D};
+use crate::Color;
+
+pub enum Pattern {
+    Color(Color),
+    Image(u32),
+}
+
+pub struct Fill {
+    path: Arc<Path>,
+    pattern: Pattern,
+    transform: usize,
+    z_index: u16,
+}
+
+pub struct Group {
+    clip: Option<Arc<Path>>,
+    // A clip rectangle in screen coordinates.
+    clip_rect: Box2D<f32>,
+    commands: Vec<Command>,
+    transform: usize,
+    z_index: u16,
+    group_stack_depth: u16,
+    can_tile_output: bool,
+    needs_surface: bool,
+}
+
+impl Default for Group {
+    fn default() -> Self {
+        Group {
+            clip: None,
+            clip_rect: Box2D::zero(),
+            commands: Vec::new(),
+            z_index: 0,
+            transform: 0,
+            group_stack_depth: 0,
+            can_tile_output: true,
+            needs_surface: false,
+        }
+    }
+}
+
+pub struct Transform {
+    transform: Transform2D<f32>,
+    parent: Option<usize>,
+}
+
+pub enum Command {
+    Fill(Fill),
+    Group(Group),
+}
+
+pub struct Commands {
+    root: Group,
+    transforms: Vec<Transform>,
+    max_group_stack_depth: u16,
+}
+
+pub struct Canvas {
+    transforms: Vec<Transform>,
+    z_index: u16,
+    current_transform: usize,
+    current_group: Group,
+    group_stack: Vec<Group>,
+    max_group_stack_depth: u16,
+}
+
+
+impl Canvas {
+    pub fn new(size: Box2D<f32>) -> Self {
+        Canvas {
+            transforms: vec![
+                Transform {
+                    transform: Transform2D::identity(),
+                    parent: None,
+                },
+            ],
+            z_index: 1,
+            current_transform: 0,
+            current_group: Group {
+                commands: Vec::new(),
+                clip: None,
+                clip_rect: size,
+                z_index: 0,
+                transform: 0,
+                group_stack_depth: 0,
+                can_tile_output: false,
+                needs_surface: true,
+            },
+            group_stack: Vec::new(),
+            max_group_stack_depth: 1,
+        }
+    }
+
+    pub fn push_transform(&mut self, transform: &Transform2D<f32>) {
+        let id = self.transforms.len();
+        if self.current_transform == 0 {
+            self.transforms.push(Transform {
+                transform: *transform,
+                parent: Some(self.current_transform),
+            });
+        } else {
+            let transform = self.transforms[self.current_transform].transform.then(transform);
+            self.transforms.push(Transform {
+                transform,
+                parent: Some(self.current_transform),
+            });
+        }
+
+        self.current_transform = id;
+    }
+
+    pub fn pop_transform(&mut self) {
+        assert!(self.current_transform != 0);
+        self.current_transform = self.transforms[self.current_transform].parent.unwrap_or(0);
+    }
+
+    pub fn fill(&mut self, path: Arc<Path>, pattern: Pattern) {
+        self.current_group.commands.push(Command::Fill(Fill {
+            path,
+            pattern,
+            transform: self.current_transform,
+            z_index: self.z_index,
+        }));
+        self.z_index += 1;
+    }
+
+    pub fn push_clip(&mut self, path: Arc<Path>) {
+        let depth = self.current_group.group_stack_depth + 1;
+        let clip_rect = self.current_group.clip_rect;
+        self.group_stack.push(std::mem::take(&mut self.current_group));
+        self.current_group.clip_rect = clip_rect;
+        self.current_group.clip = Some(path);
+        self.current_group.z_index = self.z_index;
+        self.current_group.transform = self.current_transform;
+        self.current_group.group_stack_depth = depth;
+        self.max_group_stack_depth = self.max_group_stack_depth.max(depth);
+        self.z_index += 1;
+    }
+
+    pub fn pop_clip(&mut self) {
+        let mut group = self.group_stack.pop().unwrap();
+        std::mem::swap(&mut group, &mut self.current_group);
+        assert!(group.clip.is_some());
+        self.current_group.commands.push(Command::Group(group));
+    }
+
+    pub fn finish(&mut self) -> Commands {
+        assert!(self.group_stack.is_empty());
+
+        Commands {
+            root: std::mem::take(&mut self.current_group),
+            transforms: std::mem::take(&mut self.transforms),
+            max_group_stack_depth: self.max_group_stack_depth,
+        }
+    }
+}
+
+use crate::tile_encoder::TileEncoder;
+use crate::tiler::{Tiler, TilerConfig, TiledPattern};
+use crate::gpu::masked_tiles::MaskUploader;
+
+pub struct FrameBuilder {
+    pub tile_encoders: Vec<TileEncoder>,
+    pub tiler: Tiler,
+    stats: FrameBuilderStats,
+}
+
+impl FrameBuilder {
+    pub fn new(config: &TilerConfig, uploader: MaskUploader) -> Self {
+        FrameBuilder {
+            tile_encoders: vec![TileEncoder::new(config, uploader)],
+            tiler: Tiler::new(config),
+            stats: FrameBuilderStats::new(),
+        }
+    }
+
+    pub fn build(&mut self, commands: &Commands) {
+        let t0 = time::precise_time_ns();
+
+        self.stats = FrameBuilderStats::new();
+
+        let group_stack_depth = commands.max_group_stack_depth as usize;
+        while self.tile_encoders.len() < group_stack_depth {
+            let encoder = self.tile_encoders[0].create_similar();
+            self.tile_encoders.push(encoder);
+        }
+
+        for encoder in &mut self.tile_encoders[0..group_stack_depth] {
+            encoder.reset();
+        }
+        self.tiler.clear_depth();
+
+        self.process_group(&commands.root, &commands);
+
+        for encoder in &mut self.tile_encoders[0..group_stack_depth] {
+            encoder.end_paths();
+            encoder.masked_tiles.reverse();
+        }
+
+        self.stats.total_time = Duration::from_ns(time::precise_time_ns() - t0);
+    }
+
+    pub fn process_group(&mut self, group: &Group, commands: &Commands) {
+        if let Some(clip) = &group.clip {
+            // TODO: save clip state.
+
+            // TODO: support clipping the root.
+            let parent_group_depth = group.group_stack_depth as usize - 1;
+            let encoder = &mut self.tile_encoders[parent_group_depth];
+
+            let transform = if group.transform != 0 {
+                Some(&commands.transforms[group.transform].transform)
+            } else {
+                None
+            };
+
+            self.tiler.draw.is_opaque = false;
+            self.tiler.draw.is_clip_in = true;
+
+            self.tiler.tile_path(clip.iter(), transform, encoder);
+
+            self.stats.row_time += Duration::from_ns(self.tiler.row_decomposition_time_ns);
+            self.stats.tile_time += Duration::from_ns(self.tiler.tile_decomposition_time_ns);
+
+            // TODO: tile clip
+            // TODO: push clip state.
+            self.stats.tiled_paths += 1;
+        }
+
+        for cmd in group.commands.iter().rev() {
+            match cmd {
+                Command::Fill(fill) => {
+                    let encoder = &mut self.tile_encoders[group.group_stack_depth as usize];
+                    self.tiler.draw.z_index = fill.z_index;
+                    self.tiler.draw.is_clip_in = false;
+
+                    let pattern = match fill.pattern {
+                        Pattern::Color(color) => TiledPattern::Color(color),
+                        _ => { unimplemented!() }
+                    };
+
+                    self.tiler.set_pattern(pattern);
+
+                    let transform = if fill.transform != 0 {
+                        Some(&commands.transforms[fill.transform].transform)
+                    } else {
+                        None
+                    };
+
+                    self.tiler.tile_path(fill.path.iter(), transform, encoder);
+
+                    self.stats.row_time += Duration::from_ns(self.tiler.row_decomposition_time_ns);
+                    self.stats.tile_time += Duration::from_ns(self.tiler.tile_decomposition_time_ns);
+                    self.stats.tiled_paths += 1;
+                }
+                Command::Group(group) => {
+                    self.process_group(group, commands);
+                }
+            }
+        }
+
+        if group.clip.is_some() {
+            // TODO: restore clip state.
+        }
+    }
+
+    pub fn stats(&self) -> &FrameBuilderStats {
+        &self.stats
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct Duration(u64);
+
+impl Duration {
+    pub fn from_ns(ns: u64) -> Self {
+        Duration(ns)
+    }
+
+    pub fn zero() -> Self {
+        Duration(0)
+    }
+
+    pub fn ms(self) -> f64 {
+        self.ns() as f64 / 1000000.0
+    }
+
+    pub fn ns(self) -> u64 {
+        self.0
+    }
+}
+
+use std::fmt;
+impl fmt::Debug for Duration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "{:.3}ms", self.ms())
+    }
+}
+
+impl std::ops::Add<Duration> for Duration {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Duration(self.0 + other.0)
+    }
+} 
+
+impl std::ops::AddAssign<Duration> for Duration {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct FrameBuilderStats {
+    pub tile_time: Duration,
+    pub row_time: Duration,
+    pub total_time: Duration,
+    pub tiled_paths: u32,
+}
+
+impl FrameBuilderStats {
+    pub fn new() -> Self {
+        FrameBuilderStats {
+            tile_time: Duration::zero(),
+            row_time: Duration::zero(),
+            total_time: Duration::zero(),
+            tiled_paths: 0,
+        }
+    }
+}
---
+++ tiling/src/bin/canvas.rs
@@ -0,0 +1,335 @@
+use std::sync::Arc;
+use tiling::*;
+use tiling::api::*;
+use tiling::load_svg::*;
+use tiling::gpu::masked_tiles::MaskUploader;
+use lyon::path::geom::euclid::{size2, Transform2D};
+use lyon::path::math::vector;
+
+use winit::dpi::PhysicalSize;
+use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
+use wgpu::util::DeviceExt;
+use futures::executor::block_on;
+
+fn main() {
+    profiling::register_thread!("Main");
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut use_quads = false;
+    for arg in &args {
+        if arg == "--quads" { use_quads = true; }
+    }
+
+    let tile_size = 16.0;
+    let tolerance = 0.1;
+    let scale_factor = 2.0;
+    let max_edges_per_gpu_tile = 4096;
+    let tile_atlas_size: u32 = 2048;
+
+    let mut tiler_config = TilerConfig {
+        view_box: Box2D::zero(),
+        tile_size: size2(tile_size, tile_size),
+        tile_padding: 0.5,
+        tolerance,
+        flatten: false,
+        mask_atlas_size: size2(2048, 2048),
+    };
+
+    let event_loop = EventLoop::new();
+    let window = winit::window::WindowBuilder::new()
+        .with_inner_size(winit::dpi::Size::Physical(PhysicalSize::new(1200, 1000)))
+        .build(&event_loop).unwrap();
+    let window_size = window.inner_size();
+
+    // create an instance
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+
+    // create an surface
+    let surface = unsafe { instance.create_surface(&window) };
+
+    // create an adapter
+    let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::LowPower,
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    })).unwrap();
+    // create a device and a queue
+    let (device, queue) = block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            label: None,
+            features: wgpu::Features::default(),
+            limits: wgpu::Limits::default(),
+        },
+        None,
+    )).unwrap();
+
+    let globals_ubo = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Globals"),
+        contents: bytemuck::cast_slice(&[
+            tiling::gpu::GpuGlobals {
+                resolution: vector(window_size.width as f32, window_size.height as f32),
+                tile_size: tile_size as u32,
+                tile_atlas_size,
+            }
+        ]),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+    });
+
+    let globals_bind_group_layout = tiling::gpu::GpuGlobals::create_bind_group_layout(&device);
+    let globals_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("Globals"),
+        layout: &globals_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer(globals_ubo.as_entire_buffer_binding())
+            },
+        ],
+    });
+
+    let mask_upload_copies = tiling::gpu::masked_tiles::MaskUploadCopies::new(&device, &globals_bind_group_layout);
+
+    let (view_box, paths) = if args.len() > 1 && !args[1].starts_with('-') {
+        load_svg(&args[1], scale_factor)
+    } else {
+        let mut builder = lyon::path::Path::builder();
+        builder.begin(point(0.0, 0.0));
+        builder.line_to(point(50.0, 400.0));
+        builder.line_to(point(450.0, 450.0));
+        builder.line_to(point(400.0, 50.0));
+        builder.end(true);
+
+        (Box2D { min: point(0.0, 0.0), max: point(500.0, 500.0) }, vec![(builder.build(), Color { r: 50, g: 200, b: 100, a: 255 })])
+    };
+
+    tiler_config.view_box = view_box;
+
+    let mut canvas = api::Canvas::new(tiler_config.view_box);
+
+    let mask_uploader = MaskUploader::new(&device, &mask_upload_copies.bind_group_layout, tile_atlas_size);
+
+    let mut frame_builder = FrameBuilder::new(&tiler_config, mask_uploader);
+
+    //frame_builder.tiler.set_scissor(&Box2D { min: point(500.0, 600.0), max: point(1000.0, 800.0) });
+    frame_builder.tiler.draw.max_edges_per_gpu_tile = max_edges_per_gpu_tile;
+    frame_builder.tiler.draw.use_quads = use_quads;
+
+    //frame_builder.tiler.output_is_tiled = true;
+    //frame_builder.tiler.color_tiles_per_row = 128;
+
+    canvas.push_transform(&Transform2D::translation(1.0, 1.0));
+    for (path, color) in paths {
+        canvas.fill(Arc::new(path), Pattern::Color(color));
+    }
+    canvas.pop_transform();
+
+    let mut builder = lyon::path::Path::builder();
+    builder.begin(point(0.0, 0.0));
+    builder.line_to(point(50.0, 400.0));
+    builder.line_to(point(450.0, 450.0));
+    builder.line_to(point(400.0, 50.0));
+    builder.end(true);
+    //canvas.fill(Arc::new(builder.build()), Pattern::Color(Color { r: 100, g: 200, b: 100, a: 150 }));
+
+
+    let commands = canvas.finish();
+
+    frame_builder.build(&commands);
+
+    println!("view box: {:?}", view_box);
+    println!("{} solid_tiles", frame_builder.tile_encoders[0].solid_tiles.len());
+    println!("{} alpha_tiles", frame_builder.tile_encoders[0].masked_tiles.len());
+    println!("{} gpu_masks", frame_builder.tile_encoders[0].gpu_masks.len());
+    println!("{} cpu_masks", frame_builder.tile_encoders[0].num_cpu_masks());
+    println!("{} line edges", frame_builder.tile_encoders[0].line_edges.len());
+    println!("{} quad edges", frame_builder.tile_encoders[0].quad_edges.len());
+    println!("#edge distributions: {:?}", frame_builder.tile_encoders[0].edge_distributions);
+    println!("");
+    println!("{:?}", frame_builder.stats());
+
+    let mut tile_renderer = tiling::tile_renderer::TileRenderer::new(
+        &device,
+        tile_size as u32,
+        tile_atlas_size as u32,
+        &globals_bind_group_layout,
+    );
+
+    tile_renderer.update(&mut frame_builder.tile_encoders[0]);
+
+    let mut surface_desc = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: wgpu::TextureFormat::Bgra8Unorm,
+        width: window_size.width,
+        height: window_size.height,
+        present_mode: wgpu::PresentMode::Mailbox,
+    };
+
+    surface.configure(&device, &surface_desc);
+
+    window.request_redraw();
+
+    let mut scene = SceneGlobals {
+        zoom: 1.0,
+        pan: [0.0, 0.0],
+        window_size,
+        wireframe: false,
+        size_changed: true,
+        render: true,
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        device.poll(wgpu::Maintain::Poll);
+
+        if !update_inputs(event, &window, control_flow, &mut scene) {
+            return;
+        }
+
+        if scene.size_changed {
+            scene.size_changed = false;
+            let physical = scene.window_size;
+            surface_desc.width = physical.width;
+            surface_desc.height = physical.height;
+            surface.configure(&device, &surface_desc);
+        }
+
+        if !scene.render {
+            return;
+        }
+        scene.render = false;
+
+        let frame = match surface.get_current_texture() {
+            Ok(texture) => texture,
+            Err(e) => {
+                println!("Swap-chain error: {:?}", e);
+                return;
+            }
+        };
+
+        let frame_view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        tile_renderer.begin_frame(
+            &device,
+            &queue,
+            &frame_builder.tile_encoders[0],
+            scene.window_size.width as f32,
+            scene.window_size.height as f32
+        );
+
+        queue.write_buffer(
+            &globals_ubo,
+            0,
+            bytemuck::cast_slice(&[tiling::gpu::GpuGlobals {
+                resolution: vector(
+                    scene.window_size.width as f32,
+                    scene.window_size.height as f32,
+                ),
+                tile_size: tile_size as u32,
+                tile_atlas_size,
+            }]),
+        );
+
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Tile"),
+        });
+
+        tile_renderer.render(&device, &frame_view, &mut encoder, &globals_bind_group, &mut[]);
+
+        queue.submit(Some(encoder.finish()));
+        frame.present();
+    });
+}
+
+// Default scene has all values set to zero
+#[derive(Copy, Clone, Debug)]
+pub struct SceneGlobals {
+    pub zoom: f32,
+    pub pan: [f32; 2],
+    pub window_size: PhysicalSize<u32>,
+    pub wireframe: bool,
+    pub size_changed: bool,
+    pub render: bool,
+}
+
+fn update_inputs(
+    event: Event<()>,
+    window: &Window,
+    control_flow: &mut ControlFlow,
+    scene: &mut SceneGlobals,
+) -> bool {
+    match event {
+        Event::RedrawRequested(_) => {
+            scene.render = true;
+        }
+        Event::RedrawEventsCleared => { 
+            window.request_redraw();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Destroyed,
+            ..
+        }
+        | Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+            return false;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(size),
+            ..
+        } => {
+            scene.window_size = size;
+            scene.size_changed = true
+        }
+        Event::WindowEvent {
+            event:
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(key),
+                            ..
+                        },
+                    ..
+                },
+            ..
+        } => match key {
+            VirtualKeyCode::Escape => {
+                *control_flow = ControlFlow::Exit;
+                return false;
+            }
+            VirtualKeyCode::PageDown => {
+                scene.zoom *= 0.8;
+            }
+            VirtualKeyCode::PageUp => {
+                scene.zoom *= 1.25;
+            }
+            VirtualKeyCode::Left => {
+                scene.pan[0] -= 50.0 / scene.pan[0];
+            }
+            VirtualKeyCode::Right => {
+                scene.pan[0] += 50.0 / scene.pan[0];
+            }
+            VirtualKeyCode::Up => {
+                scene.pan[1] += 50.0 / scene.pan[1];
+            }
+            VirtualKeyCode::Down => {
+                scene.pan[1] -= 50.0 / scene.pan[1];
+            }
+            VirtualKeyCode::W => {
+                scene.wireframe = !scene.wireframe;
+            }
+            _key => {}
+        },
+        _evt => {
+            //println!("{:?}", _evt);
+        }
+    }
+
+    *control_flow = ControlFlow::Poll;
+
+    true
+}
